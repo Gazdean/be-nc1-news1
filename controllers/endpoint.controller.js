53744400fb809a73325fc1502c2044a06cef9795
Gareth Dean <gazdean@hotmail.com>
2024-01-18 10:46:52 +0000

    added catch block to getAllEndpoints

--- controllers/endpoint.controller.js
+++ controllers/endpoint.controller.js
@@ -3,8 +3,8 @@ const { fetchAllEndpoints } = require("../models/endpoints.model");
 exports.getAllEndpoints = (req, res, next) => {
     fetchAllEndpoints().then((endpoints) => {
         res.status(200).send({endpoints})
-    })
-};
+    }).catch(next)
+}
 
 exports.invalidEndpoints = (req, res, next) => {
     res.status(404).send({msg: "not found, invalid endpoint"})
